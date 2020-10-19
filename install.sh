541ff3476cbb5d8526b8dd2dee41d6df293317b1
zhongxiao.yzx <zhongxiao.yzx@alibaba-inc.com>
2020-10-19 18:01:15 +0800

    [spc] set SPEC_ROOT with realpath from softlink by readlink

--- install.sh
+++ install.sh
@@ -15,9 +15,10 @@
 # limitations under the License.
 
 set -e
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 sed -i "/cli config begin/,/cli config end./ d" ~/.bashrc
 echo '#-------------------  cli config begin ----------------#' >> ~/.bashrc
-echo 'CLI_ROOT=/home/zhongxiao.yzx/workspace/cli' >> ~/.bashrc
+echo "CLI_ROOT=${SCRIPT_DIR}" >> ~/.bashrc
 echo 'PATH=${CLI_ROOT}:${CLI_ROOT}/compiler:${CLI_ROOT}/performance:${CLI_ROOT}/spec:${CLI_ROOT}/r2:${CLI_ROOT}/rpc:${PATH}' >> ~/.bashrc
 echo '#-------------------  cli config end ------------------#' >> ~/.bashrc
 
