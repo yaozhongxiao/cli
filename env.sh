a850d8b1939b903e6feebb8ca2fc73c19451a15f
yaozhongxiao <yaozhongxiao@bytedance.com>
2023-08-31 12:37:33 +0800

    [env] add os distribution variable for env

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2020 WorkGroup Participants. All rights reserved
+# Copyright 2023 WorkGroup Participants. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,13 +14,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-OS_TYPE='unknown'
-if [ "$(uname)" == "Darwin" ];then
+UNAME="$(uname | tr "[:upper:]" "[:lower:]")"
+
+OS_TYPE='unknown' # ["mac", "linux"]
+OS_DIST=${UNAME}  # ["ubuntu", "centos", "darwin"]
+if [ "${UNAME}" == "darwin" ];then
    OS_TYPE='mac'
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
+elif [ "$(expr substr ${UNAME} 1 5)" == "linux" ];then
    OS_TYPE='linux'
+   # If available, use LSB to identify distribution
+   if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+      OS_DIST=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+   # Otherwise, use release info file
+   else
+      OS_DIST=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+   fi
+    OS_DIST=$(echo ${OS_DIST} | tr [:upper:] [:lower:])
+   if [ -z "OS_DIST" ];then
+     OS_DIST=${UNAME}
+   fi
 else
-    echo "please check the os type, because it's undefined yet!"
+   echo "please check the os type, because it's undefined yet!"
 fi
 
 
@@ -34,8 +48,20 @@ case $(uname -m) in
     *) echo "please check the march, because it's undefined yet!" ;;
 esac
 
+APT="apt-get" # advanced package tool
+case "${OS_DIST}" in
+  ubuntu)
+    APT="apt-get"
+    ;;
+  centos)
+    APT="yum"
+    ;;
+  *)
+    echo "please check os distribution, because it's undefined yet!"
+    ;;
+esac
+
 xsed="sed -i"
 if [ x"${OS_TYPE}" == x"mac" ];then
   xsed="sed -i ''"
 fi
-
